9ec36fffb5d8241b3b0eb096c72b69fa8745c22d
DimasMonge <mongedimas730@gmail.com>
2024-08-28 15:17:44 -0600

    Subiendo carpetas

---
+++ CLASES-FSJ25/Javascript/sintaxis.js
@@ -0,0 +1,39 @@
+console.log("Estoy imprimiendo en consola");
+
+let suma = 2+2;
+let resta = 4-2;
+let division = 135/5;
+let multiplicacion = 1253*356;
+let residuo = 10 % 2;
+
+console.log(suma);
+
+let sumaFalsa = 2 + "2";
+
+console.log(sumaFalsa);
+
+
+
+//LLamar la funcion
+
+
+//Tipo de funciones
+//TIpo VOID  --> vacio
+function nombreFuncion(){
+    console.log("Hola te saludo desde la funcion");
+}
+
+nombreFuncion();
+
+//Tipo Return --> Decuelve valor
+function tipoReturn(){
+    return "Soy una funcion tipo Return";
+}
+
+console.log(tipoReturn());
+
+let ejecucion1 = tipoVoid();
+let ejecucion2 = tipoReturn();
+
+
+
